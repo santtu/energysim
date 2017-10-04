b522cfbd2fc3bb4daacf08f21b2f9347d3cbf04f
Santeri Paavolainen <santtu@iki.fi>
2017-10-04 11:39:41 +0300

    Added JSON decoder, that now works with scalajs too

--- build.sbt
+++ build.sbt
@@ -1,9 +1,38 @@
-// enablePlugins(ScalaJSPlugin)
+enablePlugins(ScalaJSPlugin)
 
 name := "Energy Simulation"
 scalaVersion := "2.12.2"
 
-// scalaJSUseMainModuleInitializer := true
-// libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.1"
+scalaJSUseMainModuleInitializer := true
 
+// libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.1"
+//libraryDependencies += "net.jcazevedo" %% "moultingyaml" % "0.4.0"
 libraryDependencies += "net.jcazevedo" %% "moultingyaml" % "0.4.0"
+
+val circeVersion = "0.8.0"
+
+libraryDependencies ++= Seq(
+  "io.circe" %%% "circe-core",
+  "io.circe" %%% "circe-generic",
+  "io.circe" %%% "circe-parser"
+).map(_ % circeVersion)
+
+val nlpVersion = "0.13.2"
+
+libraryDependencies  ++= Seq(
+  // Last stable release
+  "org.scalanlp" %% "breeze" % nlpVersion,
+
+  // Native libraries are not included by default. add this if you want them (as of 0.7)
+  // Native libraries greatly improve performance, but increase jar sizes.
+  // It also packages various blas implementations, which have licenses that may or may not
+  // be compatible with the Apache License. No GPL code, as best I know.
+//  "org.scalanlp" %% "breeze-natives" % nlpVersion,
+
+  // The visualization library is distributed separately as well.
+  // It depends on LGPL code
+  "org.scalanlp" %% "breeze-viz" % nlpVersion
+)
+
+
+resolvers += "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
--- src/main/scala/fi/iki/santtu/energysim/EnergySim.scala
+++ src/main/scala/fi/iki/santtu/energysim/EnergySim.scala
@@ -1,12 +1,156 @@
-import fi.iki.santtu.energysim.Model
+import java.io.{BufferedInputStream, FileInputStream}
+import java.nio.file.Files.readAllBytes
+import java.nio.file.Paths
+
+import breeze.linalg.DenseVector
+import fi.iki.santtu.energysim.{JsonDecoder, Model, YamlDecoder}
+import fi.iki.santtu.energysim.model.World
 
 object EnergySim {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+//    val data = readAllBytes(Paths.get("world.yml"))
+//    val world = Model.from(data, YamlDecoder)
+//    val data = readAllBytes(Paths.get("world.json"))
 
-    val world = Model.fromFile("world.yml")
+    val data = """{
+                 |    "areas": [
+                 |        {
+                 |            "drains": [
+                 |                {
+                 |                    "capacity": [
+                 |                        "beta",
+                 |                        500,
+                 |                        3,
+                 |                        5
+                 |                    ],
+                 |                    "name": "variant"
+                 |                }
+                 |            ],
+                 |            "name": "north",
+                 |            "sources": [
+                 |                {
+                 |                    "capacity": [
+                 |                        "uniform",
+                 |                        4000,
+                 |                        10000
+                 |                    ],
+                 |                    "ghg": 0,
+                 |                    "name": "hydro"
+                 |                }
+                 |            ]
+                 |        },
+                 |        {
+                 |            "drains": [
+                 |                {
+                 |                    "capacity": [
+                 |                        "uniform",
+                 |                        3000,
+                 |                        5000
+                 |                    ],
+                 |                    "name": "general"
+                 |                },
+                 |                {
+                 |                    "capacity": [
+                 |                        "beta",
+                 |                        5000,
+                 |                        3,
+                 |                        5
+                 |                    ],
+                 |                    "name": "variant"
+                 |                }
+                 |            ],
+                 |            "name": "south",
+                 |            "sources": [
+                 |                {
+                 |                    "capacity": [
+                 |                        "uniform",
+                 |                        750,
+                 |                        1000
+                 |                    ],
+                 |                    "ghg": 10,
+                 |                    "name": "coal"
+                 |                }
+                 |            ]
+                 |        },
+                 |        {
+                 |            "drains": [
+                 |                {
+                 |                    "capacity": [
+                 |                        "beta",
+                 |                        250,
+                 |                        3,
+                 |                        5
+                 |                    ],
+                 |                    "name": "variant"
+                 |                }
+                 |            ],
+                 |            "name": "east",
+                 |            "sources": [
+                 |                {
+                 |                    "capacity": [
+                 |                        "uniform",
+                 |                        200,
+                 |                        1000
+                 |                    ],
+                 |                    "name": "wind"
+                 |                },
+                 |                {
+                 |                    "capacity": [
+                 |                        "beta",
+                 |                        1000,
+                 |                        10,
+                 |                        2
+                 |                    ],
+                 |                    "ghg": 1,
+                 |                    "name": "gas"
+                 |                }
+                 |            ]
+                 |        }
+                 |    ],
+                 |    "lines": [
+                 |        {
+                 |            "areas": [
+                 |                "north",
+                 |                "south"
+                 |            ],
+                 |            "capacity": [
+                 |                "constant",
+                 |                5000
+                 |            ]
+                 |        },
+                 |        {
+                 |            "areas": [
+                 |                "south",
+                 |                "east"
+                 |            ],
+                 |            "capacity": [
+                 |                "constant",
+                 |                2000
+                 |            ]
+                 |        },
+                 |        {
+                 |            "areas": [
+                 |                "north",
+                 |                "east"
+                 |            ],
+                 |            "capacity": [
+                 |                "constant",
+                 |                500
+                 |            ]
+                 |        }
+                 |    ],
+                 |    "name": "simple model"
+                 |}""".stripMargin.getBytes("UTF-8")
+    val world = Model.from(data, JsonDecoder)
+
+//    val world = Model.fromFile("world.yml")
+//    val world = World("nameless")
 
     println(s"World: ${world}")
     println(s"units: ${world.units}")
+
+//    import breeze.linalg._
+    val x = DenseVector.zeros[Double](5)
+    
   }
 }
