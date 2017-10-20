8e31a2036b54f88ce660a359b6243295401e19bf
Santeri Paavolainen <santtu@iki.fi>
2017-10-20 15:31:52 +0300

    Added step capacity model

--- shared/src/main/scala/fi/iki/santtu/energysim/Model.scala
+++ shared/src/main/scala/fi/iki/santtu/energysim/Model.scala
@@ -9,6 +9,7 @@ private object CapacityConverter {
       case ("constant", Seq(value)) ⇒ ConstantCapacityModel(value.toInt)
       case ("uniform", Seq(min, max)) ⇒ UniformCapacityModel(min.toInt, max.toInt)
       case ("beta", Seq(scale, alpha, beta)) ⇒ BetaCapacityModel(scale.toInt, alpha, beta)
+      case ("step", _) => StepCapacityModel(params.grouped(3).toSeq)
 
       case (typeName, _) ⇒
         throw new Exception(s"capacity model ${typeName} not recognized")
--- shared/src/main/scala/fi/iki/santtu/energysim/model/Capacity.scala
+++ shared/src/main/scala/fi/iki/santtu/energysim/model/Capacity.scala
@@ -14,7 +14,7 @@ object ConstantCapacityModel {
 }
 
 class UniformCapacityModel(val low: Int, val high: Int) extends CapacityModel {
-  require(high > low)
+  require(high >= low)
 
   override def capacity() =
     Capacity((Random.nextDouble() * (high - low) + low).toInt)
@@ -41,4 +41,32 @@ object BetaCapacityModel {
 
 object NullCapacityModel extends ConstantCapacityModel(0) {
   def apply() = this
+}
+
+class StepCapacityModel(_steps: Seq[Seq[Double]]) extends CapacityModel {
+  val steps = {
+    val total = _steps.map(_(0)).sum
+    var upto = 0.0
+
+    _steps.map {
+      case Seq(prob, low, high) =>
+        val cprob = upto + (prob / total)
+        //println(s"prob=$prob low=$low high=$high upto=$upto total=$total cprob=$cprob")
+        upto = cprob
+        (upto, UniformCapacityModel(low.toInt, high.toInt))
+    }
+  }
+  override def capacity() = {
+    val r = Random.nextDouble()
+    val result = steps.collectFirst { case (p, m) if r <= p => m }.get.capacity()
+    //println(s"steps=${steps.toSeq} r=$r result=$result")
+    result
+  }
+
+  override def toString: String = s"Step(steps)"
+}
+
+object StepCapacityModel {
+  def apply(steps: Seq[Seq[Double]]): StepCapacityModel =
+    new StepCapacityModel(steps)
 }
