e5ca6b9ad9c3f473a3a87dc718968928365d255f
Santeri Paavolainen <santtu@iki.fi>
2017-10-27 11:31:56 +0300

    Refactored UI/browser portion to a separate sbt project
    This allows tests to run in libraryJS cleanly since they have no
    dependency on react/bootstrap/etc. in the UI.

--- build.sbt
+++ build.sbt
@@ -3,47 +3,32 @@ version := "0.1.0"
 
 scalaVersion := "2.12.2"
 
-lazy val root = project.in(file(".")).
-  aggregate(energySimulatorJS, energySimulatorJVM).
-  settings(
-    publish := {},
-    publishLocal := {}
-  )
-
 // This is to prevent me from getting confused if I accidentally typed
 // fastOptJS instead of fastOptJS::webpack -- they write to same files
 // but with different effects, resulting in confusion if used
 // interchangeably. So force this to be the webpack version.
 addCommandAlias("fastOptJS", "fastOptJS::webpack")
 
-lazy val energySimulator = crossProject.in(file("."))
+
+// root project is aggregate of the subprojects, JVM and JS libraries
+// plus UI
+lazy val root = project.in(file(".")).
+  aggregate(libraryJS, libraryJVM, ui).
+  settings(
+    publish := {},
+    publishLocal := {}
+  )
+
+// UI is a javascript single-page app project, depending on the JS
+// energysim library
+lazy val ui = (project in file("ui"))
+  .enablePlugins(ScalaJSBundlerPlugin)
   .settings(
-    libraryDependencies ++= Seq(
-      "io.circe" %%% "circe-core" % "0.8.0",
-      "io.circe" %%% "circe-generic" % "0.8.0",
-      "io.circe" %%% "circe-parser" % "0.8.0",
-      "org.scalatest" %%% "scalatest" % "3.0.1" % "test",
-      "com.outr" %%% "scribe" % "1.4.3"
-    ),
-  )
-  .jvmSettings(
-    mainClass in Compile := Some("fi.iki.santtu.energysim.Command"),
-    libraryDependencies ++= Seq(
-      "net.jcazevedo" %% "moultingyaml" % "0.4.0",
-      "com.github.scopt" %% "scopt" % "3.7.0",
-    )
-  )
-  .jsConfigure(_.enablePlugins(ScalaJSBundlerPlugin))
-  .jsSettings(
-    scalaJSUseMainModuleInitializer := true,
-//    jsEnv := new org.scalajs.jsenv.jsdomnodejs.JSDOMNodeJSEnv(),
-    mainClass in Compile := Some("fi.iki.santtu.energysim.Browser"),
-    libraryDependencies ++= Seq(
-      "org.scala-js" %%% "scalajs-dom" % "0.9.3",
-      "com.github.japgolly.scalajs-react" %%% "core" % "1.1.0",
-    ),
+    name := "Energy Simulator UI",
     webpackBundlingMode := BundlingMode.LibraryOnly(),
     webpackConfigFile := Some(baseDirectory.value / "my.webpack.config.js"),
+    mainClass in Compile := Some("fi.iki.santtu.energysimui.Main"),
+    scalaJSUseMainModuleInitializer := true,
     npmDependencies in Compile ++= Seq(
       "react" → "15.6.1",
       "react-dom" → "15.6.1",
@@ -52,6 +37,35 @@ lazy val energySimulator = crossProject.in(file("."))
       "popper.js" → "^1.12.6",
     ),
   )
+  .dependsOn(libraryJS)
 
-lazy val energySimulatorJVM = energySimulator.jvm
-lazy val energySimulatorJS = energySimulator.js
+// Library JVM and JS versions is a Scala.JS cross-project, generating
+// both JVM and JS versions. The JVM version has a command line
+// interface main provided also.
+lazy val library = crossProject.in(file("."))
+  .settings(
+    libraryDependencies ++= Seq(
+      "io.circe" %%% "circe-core" % "0.8.0",
+      "io.circe" %%% "circe-generic" % "0.8.0",
+      "io.circe" %%% "circe-parser" % "0.8.0",
+      "org.scalatest" %%% "scalatest" % "3.0.1" % "test",
+      "com.outr" %%% "scribe" % "1.4.3"
+    ),
+    name := "Energy Simulator",
+  )
+  .jvmSettings(
+    mainClass in Compile := Some("fi.iki.santtu.energysim.Command"),
+    libraryDependencies ++= Seq(
+      "net.jcazevedo" %% "moultingyaml" % "0.4.0",
+      "com.github.scopt" %% "scopt" % "3.7.0",
+    )
+  )
+  .jsSettings(
+    libraryDependencies ++= Seq(
+      "org.scala-js" %%% "scalajs-dom" % "0.9.3",
+      "com.github.japgolly.scalajs-react" %%% "core" % "1.1.0",
+    ),
+  )
+
+lazy val libraryJVM = library.jvm
+lazy val libraryJS = library.js
---
+++ ui/src/main/scala/fi/iki/santtu/energysimui/Main.scala
@@ -0,0 +1,161 @@
+package fi.iki.santtu.energysimui
+
+import fi.iki.santtu.energysim.{Model, JsonDecoder, SimulationCollector}
+import fi.iki.santtu.energysim.model.World
+import fi.iki.santtu.energysim.simulation.ScalaSimulation
+
+import scala.scalajs.js
+import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel, JSGlobal, JSImport}
+
+// @JSImport("bootstrap", JSImport.Namespace)
+// @js.native
+// object bootstrap extends js.Object {
+// }
+
+@JSExportTopLevel("EnergySim")
+object Main {
+  val data = """{
+               |    "areas": {
+               |        "east": {
+               |            "drains": [
+               |                {
+               |                    "capacity": 250,
+               |                    "name": "variant",
+               |                    "type": "city"
+               |                }
+               |            ],
+               |            "sources": [
+               |                {
+               |                    "capacity": 1000,
+               |                    "name": "wind",
+               |                    "type": "uniform"
+               |                },
+               |                {
+               |                    "capacity": 1000,
+               |                    "ghg": 1,
+               |                    "name": "gas",
+               |                    "type": "uniform"
+               |                }
+               |            ]
+               |        },
+               |        "north": {
+               |            "drains": [
+               |                {
+               |                    "capacity": 500,
+               |                    "name": "variant",
+               |                    "type": "city"
+               |                }
+               |            ],
+               |            "sources": [
+               |                {
+               |                    "capacity": 10000,
+               |                    "ghg": 0,
+               |                    "name": "hydro",
+               |                    "type": "hydro"
+               |                }
+               |            ]
+               |        },
+               |        "south": {
+               |            "drains": [
+               |                {
+               |                    "capacity": 5000,
+               |                    "name": "general",
+               |                    "type": "uniform"
+               |                },
+               |                {
+               |                    "capacity": 5000,
+               |                    "name": "variant",
+               |                    "type": "city"
+               |                }
+               |            ],
+               |            "sources": [
+               |                {
+               |                    "capacity": 1000,
+               |                    "ghg": 10,
+               |                    "name": "coal",
+               |                    "type": "uniform"
+               |                }
+               |            ]
+               |        }
+               |    },
+               |    "lines": [
+               |        {
+               |            "areas": [
+               |                "north",
+               |                "south"
+               |            ],
+               |            "capacity": 5000
+               |        },
+               |        {
+               |            "areas": [
+               |                "south",
+               |                "east"
+               |            ],
+               |            "capacity": 2000
+               |        },
+               |        {
+               |            "areas": [
+               |                "north",
+               |                "east"
+               |            ],
+               |            "capacity": 500
+               |        }
+               |    ],
+               |    "name": "simple model",
+               |    "types": {
+               |        "city": {
+               |            "data": [
+               |                3,
+               |                5
+               |            ],
+               |            "model": "beta",
+               |            "size": 0
+               |        },
+               |        "hydro": {
+               |            "data": [
+               |                [
+               |                    0.1,
+               |                    0,
+               |                    0
+               |                ],
+               |                [
+               |                    0.9,
+               |                    1,
+               |                    1
+               |                ]
+               |            ],
+               |            "model": "step",
+               |            "size": 25
+               |        }
+               |    }
+               |}""".stripMargin.getBytes("UTF-8")
+
+  @JSExport("defaultWorld")
+  val world = Model.from(data, JsonDecoder)
+
+  @JSExport
+  def run(rounds: Int, world: World = world): Unit = {
+    val simulator = ScalaSimulation
+    val collector = SimulationCollector(world)
+
+    val t0 = System.nanoTime()
+    val result = simulator.simulate(world, rounds)
+    val t1 = System.nanoTime()
+    val ms = (t1 - t0).toDouble / 1e6
+    collector += result
+
+    println(f"Simulation ${rounds} took ${ms / 1000.0}%.3f s, ${ms / rounds}%.2f ms/round")
+
+    print(SimulationCollector.summary(collector))
+  }
+
+  @JSExport
+  def main(args: Array[String]): Unit = {
+    // println(s"bootstrap: $bootstrap")
+    println(s"World: ${world}")
+    println(s"units: ${world.units}")
+
+    run(1000)
+  }
+}
